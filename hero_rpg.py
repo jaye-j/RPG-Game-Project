0f2815e0c73fba365b29eff83f766854defc61a5
Jaye Jensen <jacobrobertjensen@gmail.com>
2020-01-23 13:02:06 -0600

    rpg game project

--- hero_rpg.py
+++ hero_rpg.py
@@ -5,6 +5,9 @@
 # 1. fight goblin
 # 2. do nothing - in which case the goblin will attack him anyway
 # 3. flee
+import random
+
+
 class Character:
     def __init__(self, name, health, power):
         self.name = name
@@ -13,9 +16,9 @@ class Character:
     
     def attack(self, enemy):
         enemy.health -= self.power
-        print(f"{self.name} does {self.power} damage to the {enemy.name}.")
+        print(f"{self.name} does {self.power} damage to the {enemy.name}.\n")
         if enemy.health <= 0:
-            print(f"{enemy.name} is dead.")
+            print(f"{enemy.name} is dead.\n")
 
 
     def alive(self):
@@ -25,25 +28,38 @@ class Character:
             return True
 
     def print_status(self):
-        print(f"{self.name} has {self.health} health and {self.power} power.")
+        print(f"{self.name} has {self.health} health and {self.power} power.\n")
 
 class Hero(Character):
-    pass
-    
+    def attack(self, enemy):
+        self.damage = self.power * self.crit_multiplier()
+        print(f"{self.name} does {self.damage} damage to {enemy.name}.")
+        enemy.health -= self.damage
+        if enemy.health <= 0:
+            print(f"{enemy.name} has been defeated!")
+
+    def crit_multiplier(self):
+        #Hero has a 20% chance of doing double the damage to its opponent.
+        self.crit_multi = 1
+        if random.randint(1, 6) == 3:
+            self.crit_multi = 2
+            print(f"{self.name} lands a CRITICAL HIT!")
+        return self.crit_multi
 
 class Goblin(Character):
     pass
 
-# class Zombie(Character):
-#     def alive(self):
-#         return True
+class Zombie(Character):
+    def alive(self):
+        #Zombie never dies and while loop will never break unless flee is chosen or hero dies.
+        return True
 
 
 
 def main():
     hero = Hero("Sir Jaye the Great", 100, 100)
-    goblin = Goblin("Mudknuckle the Goblin", 20, 50)
-    
+    goblin = Goblin("Mudknuckle the Goblin", 150, 20)
+    zombie = Zombie("Graveyard Betty", 1000, 10) 
 
     while goblin.alive() and hero.alive():
         hero.print_status()
@@ -70,10 +86,46 @@ def main():
             break
 
         else:
-            print("Invalid input {}".format(raw_input))
+            print(f"Invalid input {raw_input}")
 
         if goblin.health > 0:
             # Goblin attacks hero
             goblin.attack(hero)
 
-# main()
+    print(f"{hero.name} approaches a new enemy!\n")
+
+    while zombie.alive() and hero.alive():
+        hero.print_status()
+        zombie.print_status()
+        print()
+        print("What do you want to do?")
+        print("1. fight Zombie")
+        print("2. do nothing")
+        print("3. flee")
+        print("> ", end=' ')
+
+        raw_input = input()
+
+        if raw_input == "1":
+            # Hero attacks Zombie
+            hero.attack(zombie)
+
+        elif raw_input == "2":
+            print("You're stupid! Why would you do nothing?!\n")
+            pass
+
+        elif raw_input == "3":
+            print(f"{hero.name} flees from {zombie.name}.")
+            break
+
+        else:
+            print(f"Invalid input {raw_input}")
+
+        if zombie.health > 0:
+            # Zombie attacks Hero
+            zombie.attack(hero)
+
+
+
+
+main()
